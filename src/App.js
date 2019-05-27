ffecba6ccb532ab1413376805cfc35e0c07d1a54
Kate Carter <kate.carter@tcnbroadcasting.com>
2019-05-27 13:28:47 -0600

    where is this going

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Planet, Cat } from 'react-kawaii';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lunchy</h1>
+      <Planet size={200} mood="blissful" color="#FDA7DC" />
+      <Cat size={200} mood="blissful" color="#FDA7DC" />
     </div>
   );
 }
